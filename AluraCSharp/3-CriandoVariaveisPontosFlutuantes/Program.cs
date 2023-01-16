c346d5bbeec52c3432e0a8a7cd76e431c8fd53c1
SrMarques22 <ti.brunom@gmail.com>
2023-01-16 16:54:29 -0300

    Tipos de variaveis e conversões em int C#

--- AluraCSharp/3-CriandoVariaveisPontosFlutuantes/Program.cs
+++ AluraCSharp/3-CriandoVariaveisPontosFlutuantes/Program.cs
@@ -8,6 +8,13 @@ class Programa
         double salario; //Variavel com pontos flutuante
         salario = 300.10;
         Console.WriteLine(salario);
+
+        double valor;
+        valor = 7.0 / 5; //OBS: Nesse caso, para que a variavel com Ponto Flutuante
+                         //identifique o resultado deve conter um valor com virgula
+                         //inserimos ao invés de 7/5 o valor 7.0 /5.
+        Console.WriteLine(valor);
+
         Console.WriteLine("Tecle ENTER para fechar");
         Console.ReadLine();
     }
---
+++ AluraCSharp/4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Programa
+{
+
+    static void Main(string[] args)
+    {
+        double salario = 30000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;//convertemos o double em inteiro
+        Console.WriteLine(salarioInteiro);
+
+        long x = 200000000000000000; //Para valores muito grandes temos que usar o long
+        Console.WriteLine(x);                               //ao invés do int.
+
+        short y = 1500; //Pra trabalhar com numeros menórias até 16 bits
+        Console.WriteLine(y);
+
+        double a = 30.0;
+        Console.WriteLine("O valor é "+ (int)a + "!");
+
+        float altura = 1.62f;// no c# geralmente usamos o double ao invés do Float, e para que ele
+        Console.WriteLine(altura);          // funcione, deve por um f minusculo no final do valo.
+
+        Console.WriteLine("Tecle Enter para encerrar");
+        Console.ReadLine();
+    }
+}
