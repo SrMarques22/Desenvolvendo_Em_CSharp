88746692409af0fd6489a95eecbd7d60e4b11e53
SrMarques22 <ti.brunom@gmail.com>
2023-01-16 15:39:07 -0300

    Entendendo CamelCase e variáveis C#

--- AluraCSharp/1-OlaMundo/Program.cs
+++ AluraCSharp/1-OlaMundo/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 
+//Usando o Python como base:
+//WriteLine = Print
+
+
+
 class Programa
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Olá Mundo C#!");
+        Console.WriteLine("Tecle para fechar !");
+        Console.ReadLine(); //O ReadLine vai fazer com que o sistema leia algo para prosseguir, ou seja, vai dar uma pausa ou esperar uma interação
     }
 }
---
+++ AluraCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+        int idade; //Preciso colocar o tipo de variavel antes, nesse caso, "int" para inteiro. 
+        idade = 27; // Se eu quisesse colocar o valor da idade como 3 x 2 ele traria o valor também
+        Console.WriteLine(idade);
+
+        idade = 10 + 4;
+        Console.WriteLine(idade);
+
+        idade = 280;
+        Console.WriteLine(idade);
+
+        idade = 8;
+        Console.Write(idade); //Nesse caso como exemplo, usamos apenas Write
+                                //a impressão sairá na mesma linha sem nova linha
+
+        Console.WriteLine("Tecle ENTER para fechar");
+        Console.ReadLine();
+
+    }
+}
