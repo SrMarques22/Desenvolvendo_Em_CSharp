47fca88a4ec4d8d5f07c1a0824f5c4d0f0888e05
SrMarques22 <ti.brunom@gmail.com>
2023-01-17 17:35:27 -0300

    entendendo alguns conceitos de Strings e tipos primitivos C#

---
+++ AluraCSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 6 Atribuição de Variaveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+        
+        
+        
+        
+        Console.WriteLine("Tecle para fechar !");
+        Console.ReadLine(); 
+    }
+}
